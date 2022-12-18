8dcf3ccca3acea0ec180de8735dd14275857b56c
Lexxx <simankovich@inbox.ru>
2022-12-18 18:07:30 +0400

    solution of problem 3

---
+++ HW15.12.22_3/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите число для проверки: ");
+int num = int.Parse(Console.ReadLine()!);
+Console.Clear();
+if(num % 2 == 0 ){
+    Console.WriteLine($"Число чётное: {num}");
+}
+else{
+    Console.WriteLine($"Число нечётное: {num}");
+}
