66efd7efc6295c7218e3b7eb6511b635aa4b2748
Lexxx <simankovich@inbox.ru>
2022-12-18 17:26:13 +0400

    Эsolution of problem 1

---
+++ HW15.12.22/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число 1: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число 2: ");
+int b = int.Parse(Console.ReadLine()!);
+Console.Clear();
+if (a > b){
+    Console.WriteLine($"Максимальное число: {a}");
+}
+else {
+    Console.WriteLine($"Максимальное число: {b}");
+}
