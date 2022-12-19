2e3a800b9f2ce9c79d2d090b13f24ef0ddf6fb2d
Lexxx <simankovich@inbox.ru>
2022-12-19 12:12:11 +0400

    solution of problem 4

---
+++ HW15.12.22_4/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите число для проверки: ");
+int num = int.Parse(Console.ReadLine()!);
+int num1 = 1;
+Console.Clear();
+while (num1 <= num){
+    if (num1 % 2 == 0)
+    Console.Write($"{num1} ");
+    num1++;
+}
