ce32dbe00e2e75a55c6e6e0e5de9721a6dedca6a
Lexxx <simankovich@inbox.ru>
2022-12-19 23:01:40 +0400

    добавлено решение задачи 1 от 19/12

---
+++ HW19.12.22/Program.cs
@@ -0,0 +1,10 @@
+﻿// задача 1
+Console.Write("Введите трехзначное число: ");
+int num = int.Parse(Console.ReadLine()!);
+if ((num >= 100) && (num < 1000)){
+   int result = (num - ((num/100)*100 + num%10))/10;
+   Console.WriteLine($"{result}"); 
+}
+else{
+    Console.WriteLine($"Число не является трехзначным");
+}
