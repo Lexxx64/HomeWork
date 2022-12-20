ee074b4aa69c7bd0bc60c5d7e88f22d09dfa1469
Lexxx <simankovich@inbox.ru>
2022-12-20 17:21:08 +0400

    добавлено решение задачи 3

--- HW19.12.22/Program.cs
+++ HW19.12.22/Program.cs
@@ -1,10 +1,26 @@
 ﻿// задача 1
-Console.Write("Введите трехзначное число: ");
-int num = int.Parse(Console.ReadLine()!);
-if ((num >= 100) && (num < 1000)){
-   int result = (num - ((num/100)*100 + num%10))/10;
-   Console.WriteLine($"{result}"); 
-}
+// Console.Write("Введите трехзначное число: ");
+// int num = int.Parse(Console.ReadLine()!);
+// if ((num >= 100) && (num < 1000)){
+//    int result = (num / 10 % 10);
+//    Console.WriteLine($"{result}"); 
+// }
+// else{
+//     Console.WriteLine($"Число не является трехзначным");
+// }
+
+// задача 3
+Console.Write("Введите день недели от 1 до 7: ");
+int date = int.Parse(Console.ReadLine()!);
+if ((date >= 1) && (date <= 7))
+    if ((date == 6) || (date == 7)){
+    Console.WriteLine($"Выходной");
+    }
+    else{
+    Console.WriteLine($"Будни");
+    }
 else{
-    Console.WriteLine($"Число не является трехзначным");
+    Console.WriteLine($"Нет такого дня недели");
 }
+   
+
