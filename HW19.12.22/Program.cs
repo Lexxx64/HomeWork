b35ea67ab57c1ec6f737e0d90133b216ae25cb92
Lexxx <simankovich@inbox.ru>
2022-12-21 20:03:25 +0400

    внесено решение задачи 2

--- HW19.12.22/Program.cs
+++ HW19.12.22/Program.cs
@@ -1,15 +1,34 @@
 ﻿// задача 1
-// Console.Write("Введите трехзначное число: ");
-// int num = int.Parse(Console.ReadLine()!);
-// if ((num >= 100) && (num < 1000)){
-//    int result = (num / 10 % 10);
-//    Console.WriteLine($"{result}"); 
-// }
-// else{
-//     Console.WriteLine($"Число не является трехзначным");
-// }
+
+Console.Write("Введите трехзначное число: ");
+int num = int.Parse(Console.ReadLine()!);
+if ((num >= 100) && (num < 1000)){
+   int result = (num / 10 % 10);
+   Console.WriteLine($"{result}"); 
+}
+else{
+    Console.WriteLine($"Число не является трехзначным");
+}
+
+// Задача 2
+
+Console.Write($"Введите число в интервале от 1 до 100000: ");
+int a = int.Parse(Console.ReadLine()!);
+if (a > 100000){
+    Console.WriteLine($"Упс, попробуйте снова!");
+}
+else{
+    if ((a > 99) && (a < 100001)){
+    string str = a.ToString();
+    Console.WriteLine($"{str[2]}");
+    }
+    else{
+    Console.WriteLine($"Такой цифры нет");
+    }
+}
 
 // задача 3
+
 Console.Write("Введите день недели от 1 до 7: ");
 int date = int.Parse(Console.ReadLine()!);
 if ((date >= 1) && (date <= 7))
