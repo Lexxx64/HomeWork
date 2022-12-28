a0996dd5bebcf285e54a6858fcaf0d84c2629d7d
Lexxx <simankovich@inbox.ru>
2022-12-28 20:58:13 +0400

    добавлено решение задачи 1

--- HW22.12.22/Program.cs
+++ HW22.12.22/Program.cs
@@ -1,6 +1,29 @@
-﻿// Задача 3
-Console.WriteLine("Введите число: ");
-int num = int.Parse(Console.ReadLine()!);
-for (int i = 1; i <= num; i++){
-    Console.Write($"{Math.Pow(i, 3)} ");
+﻿// Задача 1
+Console.WriteLine("Введите пятизначное число ");
+int res = int.Parse(Console.ReadLine()!);
+if ((res > 9999) && (res < 100000)){
+    int a = (res / 10000);
+    int b = (res / 1000 % 10);
+    int c = (res / 10 % 10);
+    int d = (res % 10);
+    if ((a == d) && (b == c)){
+        Console.Write($"Да детка, {res} это палиндром");
+    }
+    else {
+        Console.Write($"Мимо, {res} не палиндром");
+    }
 }
+else {
+    Console.Write($"Где тут пять знаков то?");
+}
+
+
+
+
+
+// // Задача 3
+// Console.WriteLine("Введите число: ");
+// int num = int.Parse(Console.ReadLine()!);
+// for (int i = 1; i <= num; i++){
+//     Console.Write($"{Math.Pow(i, 3)} ");
+// }
