a13095494bbfddc286f7f86a66a17c0e930a9e70
Lexxx <simankovich@inbox.ru>
2023-01-17 02:16:56 +0400

    добавлено решение задачи 34

---
+++ HW16.01.23/Program.cs
@@ -0,0 +1,22 @@
+﻿int[] Array = GetArray(45, 100, 999);
+Console.WriteLine($"В массиве [{String.Join(',', Array)}] \nколичество четных чисел равно {EvNum(Array)}");
+
+
+//----метод-создание массива-----//
+int[] GetArray(int size, int minValue, int maxValue){
+    int[] MyArray = new int[size];
+    for (int i = 0; i < size; i++){
+        MyArray[i] = new Random().Next(minValue, maxValue);
+    }
+        return MyArray;
+}
+//----метод подсчета четных чисел---//
+int EvNum(int[] Array){
+    int count = 0;
+    foreach (int el in Array){
+        if (el%2 == 0){
+            count++;
+        }
+    }
+    return count;
+}
