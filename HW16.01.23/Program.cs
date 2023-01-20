cfbfc38631ba361ff7703d87e0ee344200053f6a
Lexxx <simankovich@inbox.ru>
2023-01-20 13:49:34 +0400

    Добавлено решение задачи 37

--- HW16.01.23/Program.cs
+++ HW16.01.23/Program.cs
@@ -3,8 +3,12 @@
 // Console.WriteLine($"В массиве [{String.Join(',', Array)}] \nколичество четных чисел равно {EvNum(Array)}");
 
 //-Задача 36-//
-int[] Array = GetArray(10, -10, 10);
-Console.WriteLine($"В массиве [{String.Join(',', Array)}] \nсумма чисел в нечетных индексах равна {GetSum(Array)}");
+// int[] Array = GetArray(10, -10, 10);
+// Console.WriteLine($"В массиве [{String.Join(',', Array)}] \nсумма чисел в нечетных индексах равна {GetSum(Array)}");
+
+//-Задача 37-//
+int[] Array = GetArray(6, 1, 10);
+Console.WriteLine($"В массиве [{String.Join(',', Array)}] \nновый массив произведений пар [{String.Join(',',GetMult(Array))}]");
 
 //----метод-создание массива-----//
 int[] GetArray(int size, int minValue, int maxValue){
@@ -14,6 +18,7 @@ int[] GetArray(int size, int minValue, int maxValue){
     }
         return MyArray;
 }
+
 //----метод подсчета четных чисел---//
 int EvNum(int[] Array){
     int count = 0;
@@ -24,6 +29,7 @@ int EvNum(int[] Array){
     }
     return count;
 }
+
 //----метод суммирующий элементы нечетных ячеек----//
 int GetSum(int[] Array){
     int sum = 0;
@@ -34,3 +40,16 @@ int GetSum(int[] Array){
     }
         return sum;
 }
+
+//----метод умножения пар чисел массива----//
+int[] GetMult(int[] Array){
+    int size = Array.Length/2;
+    if (Array.Length % 2 == 1) size++;
+    int[] MyArray = new int[size];
+    for (int i = 0; i < Array.Length/2; i++){
+        MyArray[i] = Array[i] * Array[Array.Length - 1 - i];
+    }
+    if (Array.Length % 2 == 1) MyArray[size - 1] = Array[Array.Length / 2];
+    return MyArray;
+}
+
