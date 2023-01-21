98b9d6e6518840f783e58172001642d9d086d21c
Lexxx <simankovich@inbox.ru>
2023-01-21 22:32:02 +0400

    набросал метод по 41 задаче

---
+++ HW19.01.23/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] array = {0, 4, 21, 45, 46, 99, 333, 451, -151, -14};
+Console.WriteLine($"Всего в массиве больше 0 - {GetArray(array)} чисел");
+
+// 
+int GetArray(int[] array){
+    int count = 0;
+    foreach(int el in array){
+        if(el > 0){
+        count++;
+        }
+    }    
+    return count;
+}
