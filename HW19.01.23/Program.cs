1bf88a188526a1bd25cc71808c6f8aa025b26191
Lexxx <simankovich@inbox.ru>
2023-01-24 15:51:53 +0400

    найдено решение проблемы в задаче 41

--- HW19.01.23/Program.cs
+++ HW19.01.23/Program.cs
@@ -11,17 +11,37 @@
 //     }    
 //     return count;
 // }
-int[] array = {21, 4, 0, 45, 99, 46, 333, 451, -151, -14};
-    for(int i = 0; i < array.Length; i++){
-        for(int j = 0; j < array.Length - 1; j++){
-            if(array[j] > array[j+1]){
-                int x = array[j];
-                array[j] = array[j+1];
-                array[j+1] = x;
-            }
-        }
+Console.WriteLine("введите размерность массива ");
+int size = int.Parse(Console.ReadLine()!);
+int[] massive = new int[size];
+int count = 0;
+Console.WriteLine("заполните массив числами через пробел ");
+string stringArray = Console.ReadLine()!;
+string[] array = stringArray.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+for(int i = 0; i < size; i++){
+    massive[i] = Convert.ToInt32(array[i]);
     }
-Console.WriteLine(string.Join(",", array));
+    foreach(int el in massive){
+           if(el > 0){
+            count++;
+           }
+    }
+    Console.WriteLine($"{count}");
+
+
+
+
+//     for(int i = 0; i < array.Length; i++){
+//         for(int j = 0; j < array.Length - 1; j++){
+//             if(array[j] > array[j+1]){
+//                 int x = array[j];
+//                 array[j] = array[j+1];
+//                 array[j+1] = x;
+//             }
+//         }
+//     }
+// Console.WriteLine(string.Join(",", array));
 
    
     
+
