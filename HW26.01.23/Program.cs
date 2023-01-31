a3c0530a986f67c6892304cfc901326ea74d04f5
Lexxx <simankovich@inbox.ru>
2023-01-31 11:46:22 +0400

    добавлено решение задачи 54

---
+++ HW26.01.23/Program.cs
@@ -0,0 +1,39 @@
+﻿int[,] Array = GetArray(3, 4, 0, 10);
+PrintArray(Array);
+Console.WriteLine();
+SortArray(Array);
+PrintArray(Array);
+
+//---метод создание двумерного массива---
+int[,] GetArray(int a, int b, int minValue, int maxValue){
+    int[,] Array = new int[a,b];
+    for(int i = 0; i < a; i++){
+        for(int j = 0; j < b; j++){
+            Array[i,j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }return Array;
+}
+
+//---метод печати двумерного массива---
+void PrintArray(int[,] Array){
+    for(int i = 0; i < Array.GetLength(0); i++){
+        for(int j = 0; j < Array.GetLength(1); j++){
+            Console.Write($"{Array[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+//---Метод сортировки по убыванию---
+void SortArray(int[,] Array){
+    for(int i = 0; i < Array.GetLength(0); i++){
+       for(int j = 0; j < Array.GetLength(1); j++){
+          for(int k = j + 1; k < Array.GetLength(1); k++)
+            if(Array[i,k] > Array[i,j]){
+                int x = Array[i,j];
+                Array[i,j] = Array[i,k];
+                Array[i,k] = x;
+            } 
+        }
+    } 
+}
