e70983f72b66998ec032aacbbab00df8df19c123
Lexxx <simankovich@inbox.ru>
2023-01-31 18:33:05 +0400

    добавлено решение задачи 56

--- HW26.01.23/Program.cs
+++ HW26.01.23/Program.cs
@@ -1,8 +1,20 @@
-﻿int[,] Array = GetArray(3, 4, 0, 10);
+﻿//---Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит
+//по убыванию элементы каждой строки двумерного массива.
+
+// int[,] Array = GetArray(3, 4, 0, 10);
+// PrintArray(Array);
+// Console.WriteLine();
+// SortArray(Array);
+// PrintArray(Array);
+
+//---Задача 56: Задайте прямоугольный двумерный массив. 
+//Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+int[,] Array = GetArray(5, 6, 0, 10);
 PrintArray(Array);
 Console.WriteLine();
-SortArray(Array);
-PrintArray(Array);
+Console.WriteLine($" Строка {GetRowsMin(Array)} с наименьшей суммой");
+
 
 //---метод создание двумерного массива---
 int[,] GetArray(int a, int b, int minValue, int maxValue){
@@ -36,4 +48,24 @@ void SortArray(int[,] Array){
             } 
         }
     } 
+}
+
+//---метод нахождения строки с минимальной суммой---
+int GetRowsMin(int[,] Array){
+    int rows = 0;
+    int minRows = 0;
+    for(int i = 0; i < Array.GetLength(1); i++){
+        minRows += Array[0,i];
+    }
+    for(int i = 1; i < Array.GetLength(0); i++){
+        int sum = 0;
+        for(int j = 0; j < Array.GetLength(1); j++){
+            sum += Array[i,j];
+        }
+        if(minRows > sum){
+            minRows = sum;
+            rows = i;
+        }
+        }
+    return rows;
 }
