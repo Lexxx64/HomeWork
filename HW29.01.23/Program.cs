98e00a9dd7652ecc818ba8c7120d71b838e73d0b
Lexxx <simankovich@inbox.ru>
2023-02-02 23:00:31 +0400

    сделал наброски по Аккерману

---
+++ HW29.01.23/Program.cs
@@ -0,0 +1,17 @@
+﻿//---Метод функции Аккермана---
+var GetAck(int n, int m){
+    if(n > 4 && m > 2)
+    {
+        Console.Write($"Дважды подумай)");
+    }
+    return;
+    if(n == 0)
+    {
+        return m + 1;
+    }
+    if(m == 0)
+    {
+        return (n - 1, 1);
+    }
+    else (Ack(n-1, Ack(n, m - 1)));
+}
