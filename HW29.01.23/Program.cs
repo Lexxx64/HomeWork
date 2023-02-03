6fe78ec883765a589e593bc02e95b8a704d4d89b
Lexxx <simankovich@inbox.ru>
2023-02-03 16:34:15 +0400

    добавлены решения 64 и 66 задачи

--- HW29.01.23/Program.cs
+++ HW29.01.23/Program.cs
@@ -1,17 +1,51 @@
-﻿//---Метод функции Аккермана---
-var GetAck(int n, int m){
-    if(n > 4 && m > 2)
-    {
-        Console.Write($"Дважды подумай)");
-    }
-    return;
-    if(n == 0)
-    {
-        return m + 1;
-    }
-    if(m == 0)
-    {
-        return (n - 1, 1);
-    }
-    else (Ack(n-1, Ack(n, m - 1)));
+﻿//Задача 64: Выполнить с помощью рекурсии.Задайте значение N.Напишите программу,
+// которая выведет все натуральные числа в промежутке от N до 1.
+
+// Console.WriteLine("Введите число N: ");
+// int N = int.Parse(Console.ReadLine()!);
+// Console.WriteLine(GetToMin(N, 1));
+
+//Задача 66: Выполнить с помощью рекурсии.Задайте значения M и N. Напишите программу,
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+Console.WriteLine("Введите число M: ");
+int M = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число N: ");
+int N = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"Сумма натуральных элементов в промежутке от {M} до {N} \nравна {GetSumEl(M, N)}");
+
+
+//---метод рекурсии задачи 64
+string GetToMin(int start, int end)
+{
+    if (start == end) return end.ToString();
+    return (start + " " + GetToMin(start - 1, end));
 }
+
+//---Метод рекурсии задачи 66
+int GetSumEl(int start, int end)
+{
+    int sum = start;
+    if (start == end) return sum;
+    return (sum += GetSumEl(start + 1, end));
+}
+
+
+
+//---Метод функции Аккермана---
+// var GetAck(int n, int m){
+//     if(n > 4 && m > 2)
+//     {
+//         Console.Write($"Дважды подумай)");
+//     }
+//     return;
+//     if(n == 0)
+//     {
+//         return m + 1;
+//     }
+//     if(m == 0)
+//     {
+//         return (n - 1, 1);
+//     }
+//     else (Ack(n-1, Ack(n, m - 1)));
+// }
